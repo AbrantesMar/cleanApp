b7d4c4e41736a5730035d14812abd3dbeeb904e8
Márcio Abrantes <abs.marcio@gmail.com>
2022-09-02 00:55:43 -0300

    add unit test to data

---
+++ CleanApp/Data/Helpers.swift
@@ -0,0 +1,8 @@
+//
+//  Helpers.swift
+//  Data
+//
+//  Created by Marcio Henrique Nunes Abrantes on 02/09/22.
+//
+
+import Foundation
---
+++ CleanApp/Data/Helpers/ExtensionsHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionsHelpers.swift
+//  Data
+//
+//  Created by Marcio Henrique Nunes Abrantes on 02/09/22.
+//
+
+import Foundation
